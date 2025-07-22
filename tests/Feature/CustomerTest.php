5e6118b1ae06fb5d6a3f242d30543d444c649868
Danilo <fatecapi06@gmail.com>
2025-07-22 00:12:39 -0300

    dnl - Atualizado os testes de Clientes

--- tests/Feature/CustomerTest.php
+++ tests/Feature/CustomerTest.php
@@ -56,16 +56,18 @@ class CustomerTest extends TestCase
             ->assertJsonValidationErrorFor('email');
     }
 
-    public function test_nao_cria_cliente_com_email_duplicado(): void {
+    public function test_nao_cria_cliente_com_email_duplicado(): void
+    {
 
         $emailDuplicado = Customer::factory()->create();
         $novoCustomerData = Customer::factory()->make([
-            'email' => $emailDuplicado-> email])->toArray();
+            'email' => $emailDuplicado->email
+        ])->toArray();
 
         $response = $this->postJson($this->endpoint, $novoCustomerData);
 
-        $response -> assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
-            -> assertJsonValidationErrorFor('email');
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrorFor('email');
     }
 
     public function test_lista_clientes(): void
@@ -78,5 +80,83 @@ class CustomerTest extends TestCase
         //dd($response->json());
 
         $response->assertStatus(Response::HTTP_OK)->assertJsonCount(3);
+    }
+
+    public function test_detalha_cliente(): void
+    {
+        $customer = Customer::factory()->create();
+
+        $response = $this->getJson($this->endpoint . "/{$customer->id}");
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonFragment([
+                'id' => $customer->id,
+                'email' => $customer->email
+            ]);
+    }
+
+    public function test_atualiza_cliente(): void
+    {
+        $customer = Customer::factory()->create();
+
+        $atualizaData = [
+            'nome' => 'novo nome',
+            'email' => 'novoEmail@example.com'
+        ];
+
+        $response = $this->putJson("$this->endpoint/{$customer->id}", $atualizaData);
+
+        $response->assertStatus(Response::HTTP_OK)
+            ->assertJsonFragment([
+                'nome' => 'novo nome',
+                'email' => 'novoEmail@example.com'
+            ]);
+
+        $this->assertDatabaseHas('customers', [
+            'id' => $customer->id,
+            'email' => 'novoEmail@example.com'
+        ]);
+    }
+
+    public function test_exclui_cliente(): void
+    {
+        $customer = Customer::factory()->create();
+
+        $response = $this->deleteJson($this->endpoint . "/{$customer->id}");
+
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+
+        $this->assertSoftDeleted('customers', [
+            'id' => $customer->id
+        ]);
+
+        $this->assertDatabaseMissing('customers', [
+            'id' => $customer->id,
+            'deleted_at' => null
+        ]);
+    }
+
+
+    public function test_criente_nao_encontrado(): void
+    {
+        $response = $this->getJson($this->endpoint . "/99999");
+
+        $response->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    public function test_criar_Cliente_sem_dados(): void
+    {
+        $response = $this->postJson($this->endpoint, []);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonValidationErrors([
+                'nome',
+                'email',
+                'telefone',
+                'data_nascimento',
+                'endereco',
+                'bairro',
+                'cep'
+            ]);
     }
 }
